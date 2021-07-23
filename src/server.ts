ebe06d735e0a4047a57dcfddf47b8834f0e17e16
Daniel Cunha <danielcunha54@gmail.com>
2021-07-23 17:37:24 -0300

    chore: add eslint, prettier and editorconfig

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import chalk from "chalk";
+import chalk from 'chalk'
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.listen(3333, () => console.log(`Successfully started server on port ${chalk.yellow("3333")}`));
+app.listen(3333, () => console.log(`Successfully started server on port ${chalk.yellow('3333')}`))
